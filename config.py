e4ac8272c0172e05fb6a01c77f4f14552c958717
riteshbhai70 <136322164+riteshbhai70@users.noreply.github.com>
2025-11-14 05:25:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key-for-render')
-    UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER', 'SocialMediaContentAnalyzer/static/uploads')
+    UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER', 'static/uploads')
     MAX_CONTENT_LENGTH = int(os.getenv('MAX_CONTENT_LENGTH', 16 * 1024 * 1024))
